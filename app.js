0c16ecc2fbc8c44b8497fe6f7f7041e252e8c636
tarek421 <tarekmahmud740@gmail.com>
2023-07-29 14:37:22 +0600

    add vercer.json

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var admin = require("firebase-admin");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ["https://travel-agency-1.netlify.app/", "http://localhost:3000", "https://travel-agency-client-six.vercel.app/"],
+    origin: ["https://travel-agency-1.netlify.app/", "https://easy-pear-moth-fez.cyclic.app", "http://localhost:3000", "https://travel-agency-client-six.vercel.app/"],
     methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
     credentials: true
 }));
