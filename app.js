0fe59a357d21aebc0f14a2e8ebf83270c81c05b7
tarek421 <tarekmahmud740@gmail.com>
2023-07-29 10:45:00 +0600

    error solve

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://travel-agency-1.netlify.app/");
     res.setHeader(
         "Access-Control-Allow-Methods",
         "OPTIONS, GET, POST, PUT, PATCH, DELETE"
