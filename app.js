eef26a34fa37e46597cdcb12d9f44eee57f62bae
tarek421 <tarekmahmud740@gmail.com>
2023-07-29 10:36:40 +0600

    error solve

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var admin = require("firebase-admin");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://travel-agency-client-six.vercel.app/", "https://travel-agency-1.netlify.app/"],
+    origin: ["https://travel-agency-1.netlify.app/", "http://localhost:3000", "https://travel-agency-client-six.vercel.app/"],
     methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
     credentials: true
 }));
